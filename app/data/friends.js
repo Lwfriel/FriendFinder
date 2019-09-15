144e88e0a5190b1278f6d8ab3a770f75e1b9d9ac
Lauren <lwfriel@gmail.com>
2019-09-15 12:01:21 -0500

    worked on routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,33 @@
 var friendArray = [
     {
-        
+        name: "Lauren",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        name: "Patrick",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [3,2,4,4,5,5,4,3,2,1]
+    },
+    {
+        name: "Chelsea",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,1,2,1,2,4,3,1,1,1]
+    },
+    {
+        name: "Mia",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [1,2,3,4,5,5,4,3,2,1]
+    },
+    {
+        name: "Kaycee",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5,5,4,4,5,5,4,3,4,5]
+    },
+    {
+        name: "Leigh Ann",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [1,1,1,1,5,5,5,5,5,5]
     }
 ]
+module.exports = friendArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,36 @@
+var friendsData = require("../data/friends.js");
+
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
+    app.post('/api/friends', function(req, res){
+        var newFriend = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: []
+        };
+        var scoresArray = [];
+        for(var i=0; i < req.body.scores.legnth; i++){
+            scoresArray.push(parseInt(req.body.scores[i]))
+        }
+        newFriend.scores = scoresArray;
+        
+        var compareArray = [];
+        for(var i=0; i< friendsData.length; i++){
+        /// create math data to compare scores between friends
+        }
+        var closestMatch = 0; 
+        for (var i=1; i < compareArray.length; i++){
+            if (compareArray[i] <= compareArray[closestMatch]) {
+                closestMatch = i; 
+            }
+        }
+    });
+};
+app.post("/api/clear", function (req, res) {
+    surveyData.length = 0;
+    res.json({ ok: true });
+});
+
+module.exports = apiRoutes;
